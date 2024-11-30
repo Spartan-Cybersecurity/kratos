6fe043c6343c92eda1dc8302f5c7f9f7e6492f71
Spartan-Cybersecurity <76954398+Spartan-Cybersecurity@users.noreply.github.com>
2024-11-30 10:44:59 -0500

    Update sqs.py

--- sqs.py
+++ sqs.py
@@ -1,9 +1,6 @@
 import boto3
 import os
 
-#AWS_ACCESS_KEY_ID = AKIAYPUD57AE6UPR6UPD
-#AWS_SECRET_ACCESS_KEY = 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
-
 aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
 aws_region_name = "us-east-1"
